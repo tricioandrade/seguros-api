fdcb8d67b5b325cc21ab7fdba668d0bcd6888869
Patrício Andrade <tricioandrade@outlook.com>
2023-11-08 00:23:42 +0100

    :wrench: company migration created

---
+++ database/migrations/2023_09_03_215034_create_company_models_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('company', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('name');
+            $table->string('nif');
+            $table->string('address');
+            $table->string('door')->nullable();
+            $table->string('phone');
+            $table->string('email');
+
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('company');
+    }
+};
