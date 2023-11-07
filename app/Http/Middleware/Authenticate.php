38b1ca83343cd9aaef348a7b2fdfaa2774d63b7b
Patrício Andrade <tricioandrade@outlook.com>
2023-11-08 00:30:09 +0100

    :wrench: 'LoginException' handler added

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Middleware;
 
+use App\Exceptions\LoginException;
 use Illuminate\Auth\Middleware\Authenticate as Middleware;
 use Illuminate\Http\Request;
 
@@ -9,9 +10,12 @@ class Authenticate extends Middleware
 {
     /**
      * Get the path the user should be redirected to when they are not authenticated.
+     * @param Request $request
+     * @return string|null
+     * @throws LoginException
      */
     protected function redirectTo(Request $request): ?string
     {
-        return $request->expectsJson() ? null : route('login');
+        return $request->expectsJson() ? null : throw new LoginException();
     }
 }
