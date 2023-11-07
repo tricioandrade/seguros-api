ee6bbba3692e7eba7cb1b78e668ea3184f9afb2a
Patrício Andrade <tricioandrade@outlook.com>
2023-11-08 00:21:08 +0100

    :wrench: default auth model updated

--- config/auth.php
+++ config/auth.php
@@ -62,7 +62,7 @@ return [
     'providers' => [
         'users' => [
             'driver' => 'eloquent',
-            'model' => \App\Models\Users\UserModel::class,
+            'model' => \App\Models\User\UserModel::class,
         ],
 
         // 'users' => [
