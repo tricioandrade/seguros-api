35d13a4795c52d0e98374d163a8cf596e9053511
Patrício Andrade <tricioandrade@outlook.com>
2023-11-08 00:25:44 +0100

    :seedling: Exception updated

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,11 +2,17 @@
 
 namespace App\Exceptions;
 
+use App\Traits\Common\Http\HttpResponseTrait;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 use Throwable;
 
 class Handler extends ExceptionHandler
 {
+    use HttpResponseTrait;
     /**
      * The list of the inputs that are never flashed to the session on validation exceptions.
      *
@@ -26,5 +32,22 @@ class Handler extends ExceptionHandler
         $this->reportable(function (Throwable $e) {
             //
         });
+    }
+
+    /**
+     * Rendering exception response.
+     *
+     * @param Request $request
+     * @param Throwable $e
+     * @return Response|JsonResponse|\Symfony\Component\HttpFoundation\Response|RedirectResponse
+     * @throws DatabaseException
+     * @throws Throwable
+     */
+    public function render($request, Throwable $exception): Response|JsonResponse|\Symfony\Component\HttpFoundation\Response|RedirectResponse
+    {
+        if ($exception instanceof \PDOException)
+            return throw new DatabaseException($exception->getMessage());
+
+        return $this->error($exception);
     }
 }
